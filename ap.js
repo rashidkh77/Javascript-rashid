b8c1aaaf8d593f075c6648ca42e6d3d4557057d5
rashidkh77 <rashidkh100177@gmail.com>
2024-03-22 01:30:47 +0500

    Javscript Assignment

---
+++ ap.js
@@ -0,0 +1,310 @@
+// alert("Hello.. I can run JS thorugh through my web..");
+// console.log(alert);
+
+// var message ="Hello World";
+// alert(message);
+// var studentName ="Amir Khan";
+// var studentAge = "16 years Old";
+// var studentCourse = "Web Development & Mobile App";
+// alert(studentName);
+// alert(studentAge);
+// alert(studentCourse);
+// var pizza ="Pizza \n Pizz \n Piz \n Pi \n P";
+// alert(pizza);
+// var email ="rashid@gmail.com";
+// alert("My email is"+" "+email);
+// var book = "I am trying to learn from the Book A Smarter \nway to learn Javasript"
+// alert(book);
+// var script = "Yes i can write script "
+// alert(script);
+// writescript("Yes I can Write");
+// alert("Please Enter Valid Password");
+// alert("Wellcome to JS Land/nHappy coding");
+// var numb ="you have visited this site 14 times";
+// alert(nunb)
+// var lunchItems = ['Karhai','Daal','Sabzi','Biryani','Tikka']
+// var days = ["Monday","Tuesday","Wednesday","Thrusday","Friday","Satarday"]
+// var userInput = prompt("Which day You are Asking for Lunch?");
+// alert("Today Is, "  +days[0]+ "And Today We will Serve You, "+lunchItems[2])
+
+// var name; = "Wellcome to JS Land Happy Coding"
+// alert(name);
+// console.log(name);
+
+// var age = 30;
+// age = (age+10);
+// alert(age);
+// 
+// var fristName;
+// var last_name;
+// fristName="Rashid";
+// last_name="Khan";
+// var address;
+// address="Frist Street";
+// document.write(frist_name+last_name+address);
+
+// var number = 3;
+// number=number+3;
+// console.log(number);
+
+// number +=6;
+// console.log(number);
+
+// number*=6;
+// console.log(number);
+// console.log(typeof number)
+
+// var street;
+// var country;
+// street="1St Street";
+// country="Pakistan";
+// console.log(street,"  ",country);
+// var score1=10;
+// var score2=30;
+// var score3=30;
+// var totalScore=(score1+score2+score3);
+// var average=(totalScore/3);
+// console.log(totalScore, average);
+
+// Corret from ChatGPT-----////
+
+// var lunchItems = ['Karhai', 'daal', 'sabzi', 'biryani', 'Tikka'];
+// var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var userInput = prompt("Which day would you like to inquire about?");
+
+// // Find the index of the user's input in the 'days' array
+// var index = days.indexOf(userInput);
+
+// // Check if the input is a valid day
+// if (index !== -1) {
+//     alert("Today is " + days[index] + " and the lunch item is " + lunchItems[index]);
+// } else {
+//     alert("Invalid day input. Please enter a valid day.");
+// }
+
+// Class of 3-mar-24////
+//step 1----///
+// var resturentItems = [
+//     "biryani",
+//     "chicken",
+//     "masala",
+//     "qurma",
+
+// ]
+
+// var userInput = prompt("aap ko kis shelf ka khana khareed na hen");
+// alert("Congratulations Ap ne choose ki hey " + userInput
+// +"Apko Milygi " + resturentItems);
+
+// var userAge = prompt("Enter Your Age");
+// if (userAge => 18);
+// alert("Congratulations! "+ "Your Age is "+ userAge + " You have won");
+// prompt("Enter You Age");
+// if (userAge <= 18);
+// alert("Sorry! "+ "Your Age  is "+ userAge + " You have Lost");
+// console.log(user ko shelf numeber ${userInput} ka khana parcel kardo jo k ${resturentItems[userInput] hen})
+
+// var lunchItem = ['karachi', 'sabzi', 'daal', 'biryani', 'qurma', 'kusa', 'polawo'];
+
+// var days = ["monday", "tusday", "wednesday", "thirsday", "friday", "satarday", "sunday"];
+
+//var emploiyeInput = prompt(konsa din ka khana ka phonchan hen??);
+
+//step 2
+
+// if (emploiyeInput == "monday") {
+//     alert("aaj" + days[0] + "din hen, our aaj" + lunchItem[0] + "melaga");
+// }
+// if (emploiyeInput == "tusday") {
+//     alert("aaj" + days[1] + "din hen, our aaj" + lunchItem[1] + "melaga");
+// }
+// if (emploiyeInput == "wednesday") {
+//     alert("aaj" + days[2] + "din hen, our aaj" + lunchItem[2] + "melaga");
+// }
+// if (emploiyeInput == "thirsday") {
+//     alert("aaj" + days[3] + "din hen, our aaj" + lunchItem[3] + "melaga");
+// }
+// if (emploiyeInput == "friday") {
+//     alert("aaj" + days[4] + "din hen, our aaj" + lunchItem[4] + "melaga");
+// }
+// if (emploiyeInput == "satarday") {
+//     alert("aaj" + days[5] + "din hen, our aaj" + lunchItem[5] + "melaga");
+// }
+// if (emploiyeInput == "sunday") {
+//     alert("aaj" + days[6] + "din hen, our aaj" + lunchItem[6] + "melaga");
+// }
+
+//alert(aaj ${days[emploiyeInput - 1]} din hen, our aaj melayga hamen ${lunchItem[emploiyeInput -1]});
+
+//step 3
+
+// var isuserBlind = false
+//  if(!isuserBlind) {          //
+//     alert("licence aap ka huwa")
+//  }
+
+//  step -4
+
+
+// if (emploiyeInput == "friday") {
+//     alert("aaj" + days[4] + "din hen, our aaj" + lunchItem[4] + "melaga");
+// }
+// if (emploiyeInput == "satarday") {
+//     alert("aaj" + days[5] + "din hen, our aaj" + lunchItem[5] + "melaga");
+// }
+// if (emploiyeInput == "sunday") {
+//     alert("aaj" + days[6] + "din hen, our aaj" + lunchItem[6] + "melaga");
+// }
+
+//alert(aaj ${days[emploiyeInput - 1]} din hen, our aaj melayga hamen ${lunchItem[emploiyeInput -1]});
+
+//step 3
+
+// var isuserBlind = true
+//  if(!isuserBlind) {          
+//     alert("licence aap ka huwa")
+//  }
+// var  userAge = prompt("whats your age");
+
+// if (userAge >= 18){
+
+//     alert("licence ap ka hua")
+
+// }
+//     if (userAge <= 18){
+//         alert("sory aap choty ho")}
+
+// var q1 =  prompt("Capital of Pakistan?");
+// var ans1 ="Islamabad";
+// if (q1 == ans1);
+// alert("Correct Answer")
+// // var corAns =prompt(a1,ans1);
+
+// var numberOfCats = prompt("How Many Cats?");
+// var tooManyCats = numberOfCats + 1;
+// var question1 = prompt("Where Does Qauid -E Azam Was Born?");
+// if (question1 =="Karachi") {
+//     alert("Congrtulations! Your Answer is Correct");
+
+// }else alert("Sorry! your answer is wrong\n Try Next Time");
+
+// var question1 = prompt("Where Does Qauid -E Azam Was Born?");
+// if (question1 !=="Karachi") {
+//     alert(" Sorry! your answer is wrong");}
+// andif (question1 =="Karachi"){
+//     alert("Correct Answer");
+// }
+// else alert("Press any key for Try again")
+// if (time < 10) {
+//     greeting = "Good morning";
+//   } else if (time < 20) {
+//     greeting = "Good day";
+//   } else {
+//     greeting = "Good evening";
+//   
+// Chapter 3 Q.No.1
+// var age = 20;
+// alert("You are "+ age +" "+ "years Old");
+
+// var myBirthYear=1977;
+// alert("I Was Born In "+myBirthYear)
+// alert("Data Type is "+typeof myBirthYear);
+
+// Chapter No.3
+// function processOrder() 
+// {
+
+//     var visitorName = document.getElementById("visitorName").value;
+//     var itemName = document.getElementById("itemName").value;
+//     var orderQty = document.getElementById("orderQty").value;
+
+//     // Display a message in the browser
+//     var message = "Hello, " + visitorName + "! You have ordered " + orderQty + " of " + itemName + ".";
+//     alert(message);
+// }
+// var processOrder;
+// var a =10;
+// var b =5;
+// const x = function(a*b){return a,};
+// alert(a);
+
+// ------arary---///
+// var friends =['rashid','khan','danish','Bilal','Ahmed']
+// var bestfriends= friends[2];
+// console.log(friends); 
+
+// var fruit;
+// function juicerMachine(fruit){
+//     alert(`Cut ${fruit} Into Peices` );
+//     alert(`Put ${fruit} Into Juicer` );
+
+// alert("Create " + fruit + " Juice");
+// alert("Serve " + fruit +  "Juice to the Customer")
+// }
+
+// juicerMachine("Mango");
+
+// var fruitBasket=['Mango','Apple','Banana','Pineaple']
+// function juiceMaking(){
+//     prompt("Enter Your Favrate Fruit " + fruitBasket)
+// }
+// if fruitBasket =='Mango'
+// fruitBasket();
+// alert(`Enjoy your "+${fruitBasket}`)
+//Using Array and function to check user input and execute 3-sep-24
+
+// var fruitBasket = ['Mango', 'Apple', 'Banana', 'Pineapple'];
+
+// function juiceMaking() {
+//     var userFruit = prompt("Enter Your Favorite Fruit " + fruitBasket.join(', '));
+
+//     if (fruitBasket.includes(userFruit)) {
+//         alert("Enjoy your " + userFruit + " juice!");
+//     } else {
+//         alert("Sorry, we don't have that fruit in the basket.");
+//     }
+// }
+
+// juiceMaking();
+// var userName;
+// var userEmail;
+// prompt("Enter Your Name " + userName);
+// prompt("Enter Your Email " + userEmail);
+// alert("Thank you for Your Registration");
+// alert("Read Question 1 and Answer the correct")
+// var question1 = "Name of Largest City of Pakistan?";
+// var ans1 = ['Hyderabad', 'Karachi', 'Lahore', 'Peshawar', 'Islamabad'];
+// prompt(question1);
+// if (ans1 == 'Karachi'); {
+//     alert("Correct Answer");
+// }
+// alert("Read Al The Questions Carefully and chose the Correct Answer");
+
+// function quiz() {
+//     var question1 = "What  is Name of Largest City of Pakistan?";
+//     var ans1 = ['Hyderabad', 'Karachi', 'Lahore', 'Peshawar', 'Islamabad'];
+
+//     var userAnswer1 = prompt(question1 + ' (' + ans1.join(', ') + ')');
+
+//     if (ans1.includes(userAnswer1)) {
+//         alert("Correct Answer");
+//     } else {
+//         alert("Sorry, Wrong! Try Again");
+//     }
+// }
+
+// Call the quiz function
+// quiz();
+onclick= alert("Hello Rashid");
+
+/* <script>
+    // Store the string in a variable
+    var myString = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+
+    // Display the string in an alert
+    alert(myString);
+
+    // Display the string in the browser
+    document.write("<p>" + myString + "</p>");
+</script>
+
---
+++ java.js
@@ -0,0 +1,52 @@
+// alert("Hello Mr.Rashid Khan");
+// alert("! Please Enter Valid Passrowd");
+// alert("WellCome to Js Land\n Happy coding.");
+
+// alert("WellCome to Js Land");
+// alert("Happy coding.");
+// alert("Hello ...I Can run Jscript through my web console.")
+// console.log(alert);
+// var userName = ("Rkhan");
+// alert(userName);
+// console.log(userName);
+// var myName= ("Rashid Khan");
+// alert ("Hello Mr. "+ myName);
+
+// var message =("Hello World");
+// alert(message);
+// var studentName ="Bilal Khan";
+// var studentAge = "16 years Old";
+// var studentCourse = "Web Development & Mobile App";
+// alert(studentName);
+// alert(studentAge);
+// alert(studentCourse);
+// var pizza ="Pizza \n Pizz \n Piz \n Pi \n P";
+// alert(pizza);
+// var email ="rashid@gmail.com";
+// alert("My email is"+" "+email);
+// var book = "I am trying to learn from the Book A Smarter \nway to learn Javasript"
+// alert(book);
+// var message = "! Yes i can write script "
+// alert(script);
+// write.script("Yes I can Write");
+// var age = 45;
+// alert("I am "+ age + " "+ "Years Old")
+// var visit = 1;
+// var totalVisit = visit + 1;
+// alert( "You Have visited this site " + totalVisit+ " "+ "times")
+// var birthYear= 1978;
+// alert("My Birth Year "+birthYear);
+// alert("And Data Type of my decleare variable is " + typeof birthYear)
+
+// Pending to be correct///
+//  var vistorName="";
+//  var item="";
+//  var qty= 1;
+// function orderProcess(visitorName,Item,qty){;
+// var visitorName;=(prompt("Enter your name "+visitorName));
+// var item=;(prompt("Item name" +item));
+// var qty=;(prompt("Order Qty "+qty))} 
+// console.log(orderProcess());
+var userName="Ahmed";var age=10; var address="Karachi Pakistan";
+alert("Name "+ " "+ userName + age +"Years "+ "Address " +address);
+
